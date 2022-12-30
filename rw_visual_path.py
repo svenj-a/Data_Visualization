3e840d1d890f691962d1621013815b01c4cac129
Svenja <98711965+svenj-a@users.noreply.github.com>
2022-12-30 17:18:39 +0100

    Random Walk
    Refactored rw_visual and created rw_visual_path with a colored line plot.

--- rw_visual_path.py
+++ rw_visual_path.py
@@ -12,19 +12,18 @@ from random_walk import RandomWalk
 # Keep making new walks, as long as the program is active.
 while True:
     # Make a random walk.
-    rw = RandomWalk(50_000)
+    rw = RandomWalk(5_000)
     rw.fill_walk()
     
     # Plot the points in the walk.
     plt.style.use('classic')
-    fig, ax = plt.subplots(figsize=(15, 9))
+    fig, ax = plt.subplots()
     point_numbers = range(rw.num_points)
-    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.inferno,
-               edgecolors='none', s=1)
+    ax.plot(rw.x_values, rw.y_values, linewidth=1, c='gray')
     
     # Emphasize the first and last points.
     ax.scatter(0, 0, c='green', edgecolors='none', s=100)
-    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='blue', edgecolors='none',
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none',
                s=100)
     
     # Remove the axes.
