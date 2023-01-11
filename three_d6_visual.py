097b0bd1f9c8dbe6b3f9d00b3b1c63686125320b
Svenja <98711965+svenj-a@users.noreply.github.com>
2023-01-11 18:18:50 +0100

    Finished Chap 15 + Tasks
    Different simulations of rolling multiple dice saved in an html file.

--- three_d6_visual.py
+++ three_d6_visual.py
@@ -10,27 +10,30 @@ from plotly import offline
 
 from die import Die
 
-# Create a D6.
-die = Die()
+# Create three D6 dice.
+die_1 = Die()
+die_2 = Die()
+die_3 = Die()
 
 # Make some rolls, and store results in a list.
 results = []
-for roll_num in range(1000):
-    result = die.roll()
+for roll_num in range(10_000):
+    result = die_1.roll() + die_2.roll() + die_3.roll()
     results.append(result)
     
 # Analyze the results.
 frequencies = []
-for value in range(1, die.num_sides+1):
+max_result = die_1.num_sides + die_2.num_sides + die_3.num_sides
+for value in range(3, max_result+1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
 # Visualize the results.
-x_values = list(range(1, die.num_sides+1))
+x_values = list(range(3, max_result+1))
 data = [Bar(x=x_values, y=frequencies)]
 
-x_axis_config = {'title': 'Result'}
+x_axis_config = {'title': 'Result', 'dtick': 1}
 y_axis_config = {'title': 'Frequency of Result'}
-my_layout = Layout(title='Results of rolling one D6 1000 times',
+my_layout = Layout(title='Results of rolling three D6 dice 10,000 times',
                    xaxis=x_axis_config, yaxis=y_axis_config)
-offline.plot({'data': data, 'layout': my_layout}, filename='d6.html')
+offline.plot({'data': data, 'layout': my_layout}, filename='d6_d6_d6.html')
--- two_d8_visual.py
+++ two_d8_visual.py
@@ -10,27 +10,29 @@ from plotly import offline
 
 from die import Die
 
-# Create a D6.
-die = Die()
+# Create two D8 dice.
+die_1 = Die(8)
+die_2 = Die(8)
 
 # Make some rolls, and store results in a list.
 results = []
-for roll_num in range(1000):
-    result = die.roll()
+for roll_num in range(10_000_000):
+    result = die_1.roll() + die_2.roll()
     results.append(result)
     
 # Analyze the results.
 frequencies = []
-for value in range(1, die.num_sides+1):
+max_result = die_1.num_sides + die_2.num_sides
+for value in range(2, max_result+1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
 # Visualize the results.
-x_values = list(range(1, die.num_sides+1))
+x_values = list(range(2, max_result+1))
 data = [Bar(x=x_values, y=frequencies)]
 
-x_axis_config = {'title': 'Result'}
+x_axis_config = {'title': 'Result', 'dtick': 1}
 y_axis_config = {'title': 'Frequency of Result'}
-my_layout = Layout(title='Results of rolling one D6 1000 times',
+my_layout = Layout(title='Results of rolling two D8 dice 10,000,000 times',
                    xaxis=x_axis_config, yaxis=y_axis_config)
-offline.plot({'data': data, 'layout': my_layout}, filename='d6.html')
+offline.plot({'data': data, 'layout': my_layout}, filename='d8_d8.html')
